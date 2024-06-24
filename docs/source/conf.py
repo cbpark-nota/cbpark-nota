6f9c5b44e5b6697145a96c72f7a9cc5b2afb98d1
tree89 <sogogimilk@gmail.com>
2024-06-24 22:46:20 +0900

    sphinx test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,6 @@ author = 'Nota'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -22,9 +21,41 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+
 html_static_path = ['_static']
+
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
+from sphinx.locale import _
+
+sys.path.insert(0, os.path.abspath('.'))
+
+language = 'en'
+
+# 다국어 지원 설정
+locale_dirs = ['locale/']   # 번역 파일이 저장될 디렉토리
+gettext_compact = False
+
+# 확장기능 설정
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+# HTML 테마 설정
+html_theme = 'sphinx_rtd_theme'
+
+# 언어 선택기 추가 (HTML Context)
+html_context = {
+    'display_github': True,
+    'github_user': 'cbpark-nota',
+    'github_repo': 'cbpark-nota',
+    'github_version': 'sphinx/docs/',
+    'current_language': language,
+    'languages': [
+        ('en', '/cbpark-nota/'),
+        ('ko', '/cbpark-nota/ko/'),
+    ],
+}
