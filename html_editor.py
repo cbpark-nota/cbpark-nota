0e2858156721d6ab8fba0394cf11b4b07069e215
tree89 <sogogimilk@gmail.com>
2024-08-01 17:35:53 +0900

    add html editor for launguage button

--- html_editor.py
+++ html_editor.py
@@ -1,30 +1,39 @@
+from typing import Literal
+
 from bs4 import BeautifulSoup
 
 file_names = [
     "index", 
     "compatibility_range_of_conversion", 
     "compatibility_range_of_benchmark",
-    "contact_us",
+    #"contact_us",
     "error_messages",
     "int8_quantization_with_launchx",
-    "partnerships",
+    #"partnerships",
     "supported_jetpack_onnx_version"
     ]
 
-for file_name in file_names:
-    # index.html 파일 읽기
-    with open(f'docs/{file_name}.html', 'r', encoding='utf-8') as file:
-        soup = BeautifulSoup(file, 'html.parser')
+def edit_html(language:Literal["한국어", "English"], src_dir:Literal["docs/ko", "docs"]):
+    for file_name in file_names:
+        with open(f'{src_dir}/{file_name}.html', 'r', encoding='utf-8') as file:
+            soup = BeautifulSoup(file, 'html.parser')
 
-    # 'Edit on GitHub' 텍스트를 가진 <a> 태그 찾기
-    edit_link = soup.find('a', class_='fa fa-github')
+        # 'Edit on GitHub' 텍스트를 가진 <a> 태그 찾기
+        edit_link = soup.find('a', class_='fa fa-github')
 
-    # 해당 태그가 존재하면 속성 및 텍스트 변경
-    if edit_link:
-        edit_link['href'] = f'ko/{file_name}.html'
-        edit_link['class'] = 'reference internal'
-        edit_link.string = '한국어'
+        if language == "한국어":
+            href = f"ko/{file_name}.html"
+        elif language == "English":
+            href = f"{file_name}.html"
+        # 해당 태그가 존재하면 속성 및 텍스트 변경
+        if edit_link:
+            edit_link['href'] = href
+            edit_link['class'] = 'reference internal'
+            edit_link.string = language
 
-    # 변경된 내용을 index.html 파일에 다시 쓰기
-    with open(f'docs/{file_name}.html', 'w', encoding='utf-8') as file:
-        file.write(str(soup))
+        # 변경된 내용을 index.html 파일에 다시 쓰기
+        with open(f'docs/{file_name}.html', 'w', encoding='utf-8') as file:
+            file.write(str(soup))
+
+edit_html(language="한국어", src_dir="docs")
+edit_html(language="English", src_dir="docs/ko")
