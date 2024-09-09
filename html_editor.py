1107dedc05a7ac7d4b2073aca5305f6764cbfe1d
tree89 <sogogimilk@gmail.com>
2024-09-09 11:37:04 +0900

    Update for bye

--- html_editor.py
+++ html_editor.py
@@ -2,20 +2,24 @@ from typing import Literal
 
 from bs4 import BeautifulSoup
 
+
 file_names = [
-    "index", 
-    "compatibility_range_of_conversion", 
-    "compatibility_range_of_benchmark",
+    #"index", 
+    #"compatibility_range_of_conversion", 
+    #"compatibility_range_of_benchmark",
     #"contact_us",
     "error_messages",
-    "int8_quantization_with_launchx",
+    #"int8_quantization_with_launchx",
     #"partnerships",
-    "supported_jetpack_onnx_version"
+    #"supported_jetpack_onnx_version"
     ]
 
 def edit_html(language:Literal["한국어", "English"], src_dir:Literal["docs/ko", "docs"]):
     for file_name in file_names:
-        with open(f'{src_dir}/{file_name}.html', 'r', encoding='utf-8') as file:
+        
+        # with open(f'{src_dir}/{file_name}.html', 'r', encoding='utf-8') as file:
+        # ret = subprocess.PIPE("echo $PWD")
+        with open('./docs/error_messages.html', 'r', encoding='utf-8') as file:
             soup = BeautifulSoup(file, 'html.parser')
 
         # 'Edit on GitHub' 텍스트를 가진 <a> 태그 찾기
@@ -32,8 +36,9 @@ def edit_html(language:Literal["한국어", "English"], src_dir:Literal["docs/ko
             edit_link.string = language
 
         # 변경된 내용을 index.html 파일에 다시 쓰기
-        with open(f'docs/{file_name}.html', 'w', encoding='utf-8') as file:
+        with open(f'{src_dir}/{file_name}.html', 'w', encoding='utf-8') as file:
             file.write(str(soup))
 
-edit_html(language="한국어", src_dir="docs")
-edit_html(language="English", src_dir="docs/ko")
+edit_html(language="한국어", src_dir="/app/cbpark-nota/docs")
+
+#edit_html(language="English", src_dir="docs/ko")
